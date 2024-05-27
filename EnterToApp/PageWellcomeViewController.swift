94e86e12db884157eb6e14323cb83fa8ee1242d3
Birewkoss <birewkoff@gmail.com>
2024-05-27 19:27:22 +0300

    Page Welcome

---
+++ EnterToApp/PageWellcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PageWellcomeViewController.swift
+//  EnterToApp
+//
+//  Created by Артур Бирюков on 25.05.2024.
+//
+
+import UIKit
+
+class PageWellcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
